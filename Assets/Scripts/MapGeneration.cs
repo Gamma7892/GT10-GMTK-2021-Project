de173672a603b3d25c57f5c66c424e5c0fcd51d0
Michael Matthews <michaeldylanmatthews@gmail.com>
2021-06-12 06:05:23 -0400

    room generation v2. not recursive, still needs pathfinding to ensure possible route to end goal.

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class MapGeneration : MonoBehaviour
 {
-    
-    
-    
+    private float placeChance = 0.7f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Room Rooms = new Room(0);
-        Rooms.generateRooms(0, 2, 4);
+        //Random.seed = 38;
+        //generate logic map
+        int mapY = Random.Range(5, 16);
+        int mapX = Random.Range(5, 16);
+        Map map = new Map(mapY, mapX, placeChance);
+        
+        Debug.Log(map.ToString());
+        Debug.Log("Seed: " + Random.seed);
 
-        Debug.Log(Rooms.ToString());
-        Rooms.debugRooms();
+        //choose coord for start and end rooms.
     }
 
     // Update is called once per frame
@@ -23,44 +27,69 @@ public class MapGeneration : MonoBehaviour
     
 }
 
-class Room
+class Map
 {
-    public int numExits;
-    int depth = 0;
-    public List<Room> exitRooms;
+    Room[,] map;
 
-    public Room(int Depth)
+    public Map(int y, int x, float roomPlaceChance)
     {
-        exitRooms = new List<Room>();
-        depth = Depth;
+        map = new Room[y, x];
+        GenerateMap(roomPlaceChance);
     }
 
-    
-    public void generateRooms(int depth, int lowRange, int highRange)
+    public void GenerateMap(float roomPlaceChance)
     {
-        lowRange = (lowRange < 0) ? 0 : lowRange;
-        
-        numExits = Random.Range(lowRange, highRange);
-        depth++;
-        for (int i = 0; i < numExits; i++)
+        for (int i = 0; i < map.GetLength(0); i++)
         {
-            Room attachedRoom = new Room(depth);
-            exitRooms.Add(attachedRoom);
-            attachedRoom.generateRooms(depth, --lowRange, --highRange);
-        }
-    }
-
-    public void debugRooms()
-    {
-        foreach (Room r in exitRooms)
-        {
-            Debug.Log(r.ToString());
-            r.debugRooms();
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                if (Random.Range(0f, 1f) < roomPlaceChance)
+                {
+                    map[i, j] = new Room(0, i, j);
+                }
+            }
         }
     }
 
     public override string ToString()
     {
-        return ("I am a room at depth: " + depth + ", with " + numExits + " adjcent rooms.");
+        string str = "\n";
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                if (map[i,j] != null)
+                {
+                    str += "1 ";
+                }
+                else
+                {
+                    str += "0 ";
+                }
+            }
+            str += "\n";
+        }
+        return str;
+    }
+}
+
+class Room
+{
+    public int numExits;
+    //NOTE arrays throughout work in reverse what you're used to in an ordered pair. it's (y,x) NOT the euler (x,y). this is important if you want to write out a map to check your logic.
+    public int y, x;
+    public List<Room> AdjoiningRooms;
+    public GameObject thisRoom;
+
+    public Room(int Depth, int y, int x)
+    {
+        AdjoiningRooms = new List<Room>();
+        this.y = y;
+        this.x = x;
+    }
+
+    public override string ToString()
+    {
+        return ("I am a room with " + numExits + " adjcent rooms, and my coordinates are: " + y + ", " + x);
     }
 }
